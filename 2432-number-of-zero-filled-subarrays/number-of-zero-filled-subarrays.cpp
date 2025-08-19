de38f588fd5dc989e68bfee865a7ff173be4ba3f
agent-isha <yisha2001yadav@gmail.com>
2025-08-19 19:33:16 +0530

    Time: 0 ms (100.00%) | Memory: 111.3 MB (74.50%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
-        long long sum=0;
-        long long ct=0;
-        for(int i=0;i<nums.size();i++){
+        long long ans=0;
+        int ct=0;
+        for(int i=0;i<nums.size();++i){
             if(nums[i]==0){
                 ct++;
-            }else{
-                sum+=ct*(ct+1)/2;
-                ct=0;
+                ans+=ct;
+            }else {
+                    ct=0;
             }
         }
-
-        sum+=ct*(ct+1)/2;
-        return sum;
-        
-
-
-        
+        return ans;
     }
 };
