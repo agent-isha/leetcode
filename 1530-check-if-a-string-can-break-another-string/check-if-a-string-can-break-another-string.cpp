5ffc29eebf1f22ba708e00087176ffe41332c58e
agent-isha <yisha2001yadav@gmail.com>
2025-08-31 18:16:47 +0530

    Time: 35 ms (26.90%) | Memory: 14.5 MB (26.43%) - LeetSync

---
+++ 1530-check-if-a-string-can-break-another-string/check-if-a-string-can-break-another-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkIfCanBreak(string s1, string s2) {
+        sort(s1.begin(),s1.end());
+        sort(s2.begin(),s2.end());
+        bool brea=false;
+        int a=0;
+        int b=0;
+        for(int i=0;i<s1.size();i++){
+            if(s1[i]<s2[i]){
+                a+=1;
+            }
+        }
+        for(int i=0;i<s1.size();i++){
+            if(s2[i]<s1[i]){
+                b+=1;
+            }
+        }
+        if(a==0||b==0){
+            brea=true;
+        }else{
+            brea=false;
+        }
+        if(brea){
+            return true;
+        }else{
+            return false;
+        }
+
+
+        
+    }
+};
