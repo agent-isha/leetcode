af89d4016284cb58b35d055ffef5303762302636
agent-isha <yisha2001yadav@gmail.com>
2026-05-22 12:42:46 +0530

    Time: 47 ms (5.52%) | Memory: 22.7 MB (17.92%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        unordered_map<int,int>freq;
-        for(int i=0;i<nums.size();i++){
-            freq[nums[i]]++;
-
+        int a=nums.size();
+        for(int i=0;i<nums.size()-1;){
+            if(nums[i+1]==nums[i]){
+                nums.erase(nums.begin()+i+1);
+            }else{
+                i++;
+            }
         }
-        int b=nums.size();
-        int a=freq.size();
-        for (int i=freq.size();i<b;i++){
-            nums.erase(nums.begin()+i);
-
-        }
-        int i=0;
-        for(auto a:freq){
-            nums[i]=a.first;
-            
-            i++;
-        }
-        sort(nums.begin(),nums.end());
-        return a;
-
+        return nums.size();
 
         
     }
