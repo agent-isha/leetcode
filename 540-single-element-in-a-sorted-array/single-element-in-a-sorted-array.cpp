2ad7e000253fb22a4506566993f748b13da63531
agent-isha <yisha2001yadav@gmail.com>
2026-06-02 13:49:14 +0600

    Time: 0 ms (100.00%) | Memory: 26.3 MB (33.09%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -3,18 +3,43 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         int lo=0;
         int high=nums.size()-1;
-        while(lo<high){
+        int n=nums.size()-1;
+        int ans=-1;
+
+        if(nums.size()==1){
+            return nums[0];
+        }
+
+        while(lo<=high){
             int mid=lo+(high-lo)/2;
-            if(mid%2==1){
-                mid--;
+
+            if((mid==0 || nums[mid]!=nums[mid-1]) &&
+               (mid==n || nums[mid]!=nums[mid+1])){
+                ans=nums[mid];
+                break;
             }
-            if(nums[mid]==nums[mid+1]){
-                lo=mid+2;
+
+            if(mid%2==0){
+                if(mid!=n && nums[mid]==nums[mid+1]){
+                    lo=mid+2;
+                }else if(mid==n){
+                    ans=nums[mid];
+                    break;
+                }else{
+                    high=mid-1;
+                }
             }else{
-                high=mid;
+                if(mid!=0 && nums[mid]==nums[mid-1]){
+                    lo=mid+1;
+                }else if(mid==0){
+                    ans=nums[mid];
+                    break;
+                }else{
+                    high=mid-1;
+                }
             }
         }
-        return nums[lo];
-        
+
+        return ans;
     }
 };
