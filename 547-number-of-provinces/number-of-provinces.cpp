6062825c53a71fe6c8f386a93a55eaa33e5e0d74
agent-isha <yisha2001yadav@gmail.com>
2026-06-07 00:54:24 +0600

    Time: 4 ms (25.46%) | Memory: 20.3 MB (41.00%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void dfs(int node, vector<int> adjls[], vector<int>& vis) {
+        vis[node] = 1;
+
+        for(auto it : adjls[node]) {
+            if(!vis[it]) {
+                dfs(it, adjls, vis);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& adj) {
+        int n = adj.size();
+
+        vector<int> adjls[n];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(adj[i][j] == 1 && i != j) {
+                    adjls[i].push_back(j);
+                }
+            }
+        }
+
+        vector<int> vis(n, 0);
+
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(!vis[i]) {
+                cnt++;
+                dfs(i, adjls, vis);
+            }
+        }
+
+        return cnt;
+    }
+};
