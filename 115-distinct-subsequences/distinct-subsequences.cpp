abc16068322ac1061f185f76b8ca9139b1669dcf
agent-isha <yisha2001yadav@gmail.com>
2026-08-11 15:09:37 +0530

    Time: 39 ms (21.15%) | Memory: 27 MB (54.22%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int f(int i,int j,string &s,string &t,vector<vector<int>>&dp){
-        if(j == t.size())
+        if(j <0)
             return 1;
 
-        if(i == s.size())
+        if(i<0)
             return 0;
 
         if(dp[i][j] != -1)
             return dp[i][j];
 
         if(s[i] == t[j]) {
-            return dp[i][j] = f(i + 1, j + 1, s, t, dp)
-                             + f(i + 1, j, s, t, dp);
+            return dp[i][j] = f(i - 1, j -1, s, t, dp)
+                             + f(i - 1, j, s, t, dp);
         }
 
-        return dp[i][j] = f(i + 1, j, s, t, dp);
+        return dp[i][j] = f(i -1, j, s, t, dp);
     }
         
     
@@ -25,7 +25,7 @@ public:
         
         int ans=0;
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(0,0,s,t,dp);
+        return f(n-1,m-1,s,t,dp);
 
         
     }
