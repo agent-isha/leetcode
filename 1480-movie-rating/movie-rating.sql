c5822d9d1f296d0a200330d57a70878d36829dc8
agent-isha <yisha2001yadav@gmail.com>
2026-08-21 18:35:29 +0530

    Time: 1798 ms (15.27%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1480-movie-rating/movie-rating.sql
@@ -0,0 +1,26 @@
+# Write your MySQL query statement below
+SELECT name AS results
+FROM (
+    SELECT u.name, COUNT(*) AS cnt
+    FROM Users u
+    JOIN MovieRating mr 
+    ON u.user_id = mr.user_id
+    GROUP BY u.user_id, u.name
+    ORDER BY cnt DESC, u.name
+    LIMIT 1
+) a
+
+UNION ALL
+
+SELECT title AS results
+FROM (
+    SELECT m.title, AVG(mr.rating) AS avg_rating
+    FROM Movies m
+    JOIN MovieRating mr ON 
+    m.movie_id = mr.movie_id
+    WHERE mr.created_at >= '2020-02-01'
+    AND mr.created_at < '2020-03-01'
+    GROUP BY m.movie_id, m.title
+    ORDER BY avg_rating DESC, m.title
+    LIMIT 1
+) b;
